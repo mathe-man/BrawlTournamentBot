5c3b85ded6252b76d62558fb49da671358559a6a
mathe-man <mathieu.workcontact@gmail.com>
2025-03-25 20:09:59 +0300

    Add Commands.cs for bot commands, add troll to it, create second Log method with string as parameter

---
+++ BrawlTournamentBot/Commands.cs
@@ -0,0 +1,36 @@
+using Discord;
+using Discord.WebSocket;
+using Discord.Interactions;
+
+namespace BrawlTournamentBot;
+
+public class Commands : InteractionModuleBase<SocketInteractionContext>
+{
+    [SlashCommand("say", "Fait dire quelque chose au bot")]
+    public async Task Say([Summary("Texte", "Le texte qu'il écrira")] string text,
+        [Summary("channel", "le channel où il écrira le message")] ISocketMessageChannel? channel = null)
+    {
+        if (Context != null) //If is called from discord
+        {
+            if(Equals(channel, null))   channel = Context.Channel;
+            
+            await channel.SendMessageAsync(text);
+        }   
+        else                //Is called from the code
+        {
+            
+        }
+    }
+    
+    [SlashCommand("troll", "Ajoute une nom ensuite pour le troll")]
+    public async Task Troll([Summary("nom", "le nom à troller")] string name)
+    {
+        if (name.ToLower().Contains("math"))
+        {
+            await Say($"Mathe-man en vrai il est pas si mal,\nmais {Context.User.Username} il a le niveau d'une poubelle", Context.Channel);
+            return;
+        }
+
+        await Say($"{name} est grave nul en vrai");
+    }
+}
--- BrawlTournamentBot/Program.cs
+++ BrawlTournamentBot/Program.cs
@@ -28,40 +28,23 @@ class Program
 
     private Task Log(LogMessage msg)
     {
-        Console.WriteLine(msg.ToString());
+        string log = $"[{DateTime.Now}]: {msg.ToString()}";
+        Console.WriteLine(log);
         return Task.CompletedTask;
     }
+
+    private Task Log(string msg)
+    {
+        string log = $"[{DateTime.Now}]: {msg.ToString()}";
+        Console.WriteLine(log);
+        return Task.CompletedTask;
+    }
+    
 
     private async Task MessageReceivedAsync(SocketMessage message)
     {
         if (message.Author.IsBot || message.Author.IsWebhook || !message.Content.StartsWith("/")) return;
-        string[] command = message.Content.Remove(0, 1).Split(" ");
-
-        if (command[0] == "troll")
-        {
-            if (command.Length == 1)
-            {await SendMessage("Tu parle de qui ??", message.Channel); return;}
-            
-            if (command[1].ToLower().Contains("math"))
-            {
-                await SendMessage("Mathe-man en vrai c'est le goat", message.Channel);
-                await Troll(message.Author.Username, message.Channel);
-                return;
-            }
-
-            await Troll(command[1], message.Channel);
-        }
-        
     }
 
-    private async Task SendMessage(string message, ISocketMessageChannel channel)
-    {
-        await channel.SendMessageAsync(message);
-    }
     
-    //Commands
-    private async Task Troll(string name, ISocketMessageChannel channel)
-    {
-        await SendMessage($"{name} est plutôt nul sur le jeu",  channel);
-    }
 }
